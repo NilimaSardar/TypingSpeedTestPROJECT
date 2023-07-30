3486555c464e71ec65c08fcff423abddd8baab7f
Nilima <snilima181@gmail.com>
2023-07-30 17:29:35 +0545

    index.html to index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                Typing-test
+            </div>
+            <div class="menu">
+                <a href="#">Home</a>
+                <a href="#">Tests</a>
+                <a href="#">About</a>
+                <a href="#">Themes</a>
+                <a href="#">Lang</a>
+            </div>
+            <div class="login_reg">
+                <a href="signup.php">Register</a>
+            </div>
+        </nav>
+        <section class="h-txt">
+            <span>Hello!</span>
+            <h1>Welcome to Typing World</h1>
+
+            <br>
+            <a href="login.php">Login</a>
+        </section>
+    </header>
+</body>
+</html>
